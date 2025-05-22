15be632797977cbf40c830165b12dd67912f9f66
emartin <eamart34@uw.edu>
2025-05-22 10:28:49 -0700

    updating home page guidelines

--- src/home.tsx
+++ src/home.tsx
@@ -6,23 +6,21 @@ const Home: React.FC = () => {
     <div className="max-w-4xl mx-auto mt-10 p-6 border rounded-lg shadow-lg bg-white">
       <h1 className="text-3xl font-bold text-center mb-6">Welcome to AuditAI</h1>
       <p className="text-lg mb-4">
-        AuditAI is a tool designed to help give users the transparency they deserve in evaluating the relevancy, quality, and biases found in 
-        an answer that they received as part of asking a question, either to an AI or another source. 
+        AuditAI is a tool designed to help give users the transparency and verifiability they deserve in evaluating 
+        an answer they received from asking a question, either to an LLM or another source
       </p>
       <h2 className="text-2xl font-semibold mb-4">How It Works</h2>
       <ul className="list-disc list-inside mb-4">
-        <li>Enter a question and answer in the Audit tool.</li>
-        <li>You will receive a quality score including the question similarity (how relevant is the answer to the original question)</li>
-        <li>You will receive a bias score including different types of biases that your answer might contain</li>
+        <li>Enter a question and answer in the Audit tool</li>
+        <li>Choose your sources you want to audit with</li>
+        <li>Receive back audit information</li>
       </ul>
       <h2 className="text-2xl font-semibold mb-4">Guidelines</h2>
       <ul className="list-disc list-inside mb-4">
-        <li>Ensure that the question and answer are related to standard English language applications</li>
-        <li>This tool is NOT intended to work with code or any application outside of the standard English language</li>
-        <li>Feel free to copy over the answer you wanted audited in its entirety. But beware that some of the bias models
-            accept a limited amount of text due to complexity, so make sure that the most relevant part of the answer you want 
-            audited appears first when submitting the answer. 
+        <li>Ensure that the question and answer are related to standard English language applications and could be verified on the internet</li>
+        <li>Feel free to copy over the answer you wanted audited in its entirety. But beware that the tool functions best with summarized sentences that are shorter in length
         </li>
+        <li>Always make sure to verify the information presented to you in AuditAI on your own. This is not a definitive audit solution, and should be independently verified</li>
       </ul>
       <p className="text-lg mb-4">
         If you're ready to audit your question and answer pair, navigate to the <Link to="/audit" className="text-blue-500 hover:underline">Audit</Link> page.
